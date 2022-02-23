036fd42848106476131065e69542c0f872d9f3de
Anupam Dahal <anupamdahal1@gmail.com>
2022-02-23 11:29:49 -0600

    Undo dataset changes

--- src/data/metadata/dataset.js
+++ src/data/metadata/dataset.js
@@ -1,5 +1,5 @@
 const userStore = {
-  dhis_dist_version : "guinea_1",
+  dhis_dist_version : "guinea_2",
   vegetationDataElementId : "bUBLqMJTkp4",
   temperatureDataElementId : "afWPpfBwIaa",
   precipitationDataElementId : "oYJ51K7AJvQ"
